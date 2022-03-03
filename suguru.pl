1e3777de620cde26c74faf322af785cfb65ff843
arthurwinck <apwinck@gmail.com>
2022-03-03 11:00:07 -0300

    maplists

--- suguru.pl
+++ suguru.pl
@@ -11,14 +11,13 @@ matriz(
     [ponto(0,3),ponto(0,7),ponto(0,7),ponto(0,7),ponto(0,7),ponto(0,5)]
 ]
 ).
-
 %! Seta o elemento
 
 %! retorna um ponto a partir de um valor e uma região
 criarPonto(V, R, ponto(V, R)) :- !.
 
 %! retorna uma coordenada a partir de i e j especificados
-criarCoord(i, j, coord(i,j)) :- !.
+criarCoord(I, J, coord(I,J)) :- !.
 
 %! Retorna a região do ponto específico
 getR(ponto(_,R), R).
@@ -28,10 +27,9 @@ getV(ponto(V,_), V).
 
 %! retorna um ponto (valor e região) / Testa os limites da matriz. Caso não satisfaça a condição,
 %! retorna um ponto inválido
-getP(coord(I,J), matriz, P) :-
+getP(coord(I,J), M, P) :-
     (
-    matriz(M),
-    length(M, L1), I > 0, L1 >= I, nth0(0, M, L),
+    length(M, L1), I >= 0, L1 > I, nth0(0, M, L),
     length(L, L2), J >= 0, L2 > J, nth0(I, M, LI),
     nth0(J, LI, P), !
     );
@@ -53,33 +51,67 @@ setaElemento([H|T], K, elem, [H|L]) :-
 setaP(coord(I, J), P, M, ML) :-
     nth0(I, M, L),
     replace(L, J, P, NL),
-    replace(M, I, P, ML), !.
+    replace(M, I, NL, ML), !.
 
 %! Retorna a região do ponto na coordenada
-getRAux(coord(I,J), matriz, R) :-
-    getP(coord(I,J), matriz, P),
-    getR(P,G),
-    matriz(M),
+getRAux(C, M, R) :-
+    getP(C, M, P),
+    getR(P, G),
     findall(
         C1,
         (
-            nth0(0,M,L),
+            nth0(0, M, L),
             length(M, MI1), length(L, MJ1),
-            MI2 is MI1 -1, MJ2 is MJ1 - 1,
-            between(0, MI2, I), between(0, MJ2, J),
-            criarCoord(I, J, C1), getP(C1, M, P1), getR(P1,G1), G1 == G
-        ),
+            MI is MI1 - 1, MJ is MJ1 - 1,
+            between(0, MI, I), between(0, MJ, J),
+            criarCoord(I, J, C1), getP(C1, M, P1), getR(P1, G1), G1 == G
+            ),
         R
     ).
 
+getVizinhos(coord(I,J), V) :-
+    criarCoord(I,J,CN),
+    findall(
+        C,
+        (   IH is I + 1, IL is I - 1,
+            JH is J + 1, JL is I - 1,
+            between(IL, IH, I1), between(JL, JH, J1),
+            criarCoord(I1, J1, C), C \== CN
+        ),
+        V
+    ).
 
+getRFromLista(L, R) :-
+	maplist(getR, L, R).
+
+getVizFromLista(L, LViz) :-
+    maplist(getVizFromListaAux, L, LViz).
+
+getVizFromListaAux(coord(I,J), V) :-
+    criarCoord(I,J,CN),
+    findall(
+        C,
+        (   IH is I + 1, IL is I - 1,
+            JH is J + 1, JL is I - 1,
+            between(IL, IH, I1), between(JL, JH, J1),
+            criarCoord(I1, J1, C), C \== CN
+        ),
+        V
+    ).
+
+suguru(matriz):-
+    matriz(M),
+    length(M,6),
+    maplist(same_length(M), M).
+    
+    
 sudoku(Rows) :-
     length(Rows, 6),
     maplist(same_length(Rows), Rows),
     append(Rows, Vs), Vs ins 1..6,
     maplist(all_distinct, Rows),
     transpose(Rows, Columns),
-    maplist(all_distinct, Columns).
+	maplist(all_distinct, Columns).
 
 blocks([], [], []).
 blocks([N1,N2,N3|Ns1], [N4,N5,N6|Ns2], [N7,N8,N9|Ns3]) :-
