dc0995fee0abf5a1d89e8c3936e30349b259738c
arthurwinck <apwinck@gmail.com>
2022-03-05 15:27:16 -0300

    Execução funcionando (em loo)

--- suguru.pl
+++ suguru.pl
@@ -1,16 +1,5 @@
 :- use_module(library(clpfd)).
 
-problem(1, [[_,_,_,_,_,_,_,_,_],
-            [_,_,_,_,_,3,_,8,5],
-            [_,_,1,_,2,_,_,_,_],
-            [_,_,_,5,_,7,_,_,_],
-            [_,_,4,_,_,_,1,_,_],
-            [_,9,_,_,_,_,_,_,_],
-            [5,_,_,_,_,_,_,7,3],
-            [_,_,2,_,1,_,_,_,_],
-            [_,_,_,_,4,_,_,_,9]]).
-
-%! Instanciação da matriz
 matriz(
 [
     [ponto(4,0),ponto(0,0),ponto(0,1),ponto(0,2),ponto(0,2),ponto(0,2)],
@@ -22,94 +11,59 @@ matriz(
 ]
 ).
 
-matriz2(
-[
-    [ponto(4,0),ponto(_,0),ponto(_,1),ponto(_,2),ponto(_,2),ponto(_,2)],
-    [ponto(_,0),ponto(_,1),ponto(_,1),ponto(_,1),ponto(_,2),ponto(_,5)],
-    [ponto(_,4),ponto(_,3),ponto(4,1),ponto(_,4),ponto(_,2),ponto(1,5)],
-    [ponto(_,3),ponto(_,3),ponto(_,4),ponto(2,4),ponto(_,4),ponto(_,5)],
-    [ponto(5,3),ponto(_,6),ponto(_,6),ponto(3,4),ponto(5,7),ponto(_,5)],
-    [ponto(_,3),ponto(_,7),ponto(_,7),ponto(_,7),ponto(_,7),ponto(_,5)]
-]
-).
-%! Seta o elemento
 
-%! retorna um ponto a partir de um valor e uma região
+% Instancia um ponto com valor V e região R
 criarPonto(V, R, ponto(V, R)) :- !.
 
-%! retorna uma coordenada a partir de i e j especificados
-criarCoord(I, J, coord(I,J)) :- !.
+% cria uma coordenada relacionada a um ponto com I e J
+criarCoord(I, J, coord(I, J)) :- !.
 
-%! Retorna a região do ponto específico
-getR(ponto(_,R), R).
-
-%! Retorna o valor do ponto específico
-getV(ponto(V,_), V).
-
-%! retorna um ponto (valor e região) / Testa os limites da matriz. Caso não satisfaça a condição,
-%! retorna um ponto inválido
-getP(coord(I,J), M, P) :-
+% Recebe uma coordenada I,J e a matriz e retorna o ponto presente nessa coordenada
+getP(coord(I, J), M, P) :-
     (
-    length(M, L1), I >= 0, L1 > I, nth0(0, M, L),
-    length(L, L2), J >= 0, L2 > J, nth0(I, M, LI),
-    nth0(J, LI, P), !
+        length(M, Z1),
+        I < Z1,
+        I < 6,
+    	I >= 0,
+        nth0(0, M, L),
+        length(L, Z2),
+        J < Z2,
+        J < 6,
+        J >= 0,
+        nth0(I, M, Li),
+        nth0(J, Li, P),
+        !
     );
-    criarPonto(-1,-1, P), !.
+    criarPonto(-1, -1, P),
+    !.
 
-%! seta o elemento no index I recursivamente
+% seta o elemento P no indíce K, esse é o método base
+setaElemento([_|T], K, P, [P|T]) :-
+    K == 0,
+    !.
 
-%! Caso base
-setaElemento([_|T], 0, elem, [elem|T]) :- !.
+% Método recursivo de seta elemento
+setaElemento([H|T], K, P, [H|L]) :-
+    K1 is K - 1,
+    setaElemento(T, K1, P, L),
+    !.
 
-%! Caso normal
-setaElemento([H|T], K, elem, [H|L]) :-
-    KS is K-1,
-    setaElemento(T, KS, elem, L).
-
-%! seta P na coordenada i,j
-%! Primeiro seta o elemento na lista e
-%! após isso seta a nova lista dentro na matriz
-setaP(coord(I, J), P, M, ML) :-
+% Seta um ponto na coordenada I, J da matriz M
+setaP(coord(I, J), P, M, NM) :-
     nth0(I, M, L),
-    replace(L, J, P, NL),
-    replace(M, I, NL, ML), !.
+    setaElemento(L, J, P, NL),
+    setaElemento(M, I, NL, NM),
+    !.
 
-%! Retorna a região do ponto na coordenada
-getRAux(C, M, R) :-
-    getP(C, M, P),
-    getR(P, G),
-    findall(
-        C1,
-        (
-            nth0(0, M, L),
-            length(M, MI1), length(L, MJ1),
-            MI is MI1 - 1, MJ is MJ1 - 1,
-            between(0, MI, I), between(0, MJ, J),
-            criarCoord(I, J, C1), getP(C1, M, P1), getR(P1, G1), G1 == G
-            ),
-        R
-    ).
+% A partir de um ponto retorna o seu valor
+getV(ponto(V, _), V).
+% A partir de um ponto retorna a sua região
+getR(ponto(_, R), R).
 
-
-
-getVizinhos(coord(I, J), NB) :-
-    criarCoord(I, J, C1),
-    findall(
-        C,
-        (
-            Imax is I + 1, Imin is I - 1,
-            Jmax is J + 1, Jmin is J - 1,
-            between(Imin, Imax, I1), between(Jmin, Jmax, J1),
-            criarCoord(I1, J1, C), C \== C1
-        ),
-        NB
-    ).
-
-
-
-getRegiaoFromMatriz(C, M, R) :-
+% A partir de uma região da coordenada C (I, J), retorna uma lista de coordenadas que possuem a mesma região
+getRegiaoFromMatriz(C, M, Rs) :-
 	getP(C, M, P),
-    getR(P, G),
+    getR(P, R),
 	findall(
         C1,
         (
@@ -117,60 +71,134 @@ getRegiaoFromMatriz(C, M, R) :-
             length(M, MI1), length(L, MJ1),
             MI is MI1 - 1, MJ is MJ1 - 1,
             between(0, MI, I), between(0, MJ, J),
-            criarCoord(I, J, C1), getP(C1, M, P1), getR(P1, G1), G1 == G
+            criarCoord(I, J, C1), getP(C1, M, P1), getR(P1, R1), R1 == R
             ),
-        R
+        Rs
     ).
 
 
-acessarMatriz(M, I, J, V) :-
-    nth0(I, M, L),
-    nth0(J, L, V).
-
-getRFromMatriz(M, R) :-
-    maplist(getRFromLista, M, R).
-
-getRFromLista(L, R) :-
-	maplist(getR, L, R).
-
-getVFromMatriz(M,R) :-
-    maplist(getVFromLista, M, R).
-
-getVFromLista(L,R) :-
-    maplist(getV, L, R).
-
-getVizFromMatriz(M, MViz) :-
-    maplist(getVizFromLista, M, MViz).
-
-getVizFromLista(L, LViz) :-
-    maplist(getVizFromCoord, L, LViz).
-
-getVizFromCoord(coord(I,J), V) :-
-    criarCoord(I,J,CN),
+% retorna uma lista de coordenadas vizinhas a coordenada repassada
+getVizinhos(coord(I, J), Viz) :-
+    criarCoord(I, J, C1),
     findall(
         C,
-        (   IH is I + 1, IL is I - 1,
-            JH is J + 1, JL is I - 1,
+        (
+            IH is I + 1, IL is I - 1, JH is J + 1, JL is J - 1,
             between(IL, IH, I1), between(JL, JH, J1),
-            criarCoord(I1, J1, C), C \== CN
+            criarCoord(I1, J1, C), C \== C1
         ),
-        V
+        Viz
     ).
 
-verificaGrupo(C, N, M) :-
-    getRAux(C, M, R),
-    verificaLista(R, N, M), !.
+% Retorna true caso o valor repassado não esteja presente na sua região e false se
+% já estiver presente
+verificaRegiao(C, N, M) :-
+    getRegiaoFromMatriz(C, M, R),
+    verificaLista(R, N, M),
+    !.
 
-verificaVizinhos(coord(I,J), N) :-
-    getVizinhos(coord(I,J), R),
-    verificaLista(R, N), !.
+% Retorna true se o valor não está presente nas adjacências da coordenada C e false caso estiver
+verificaVizinhos(C, N, M) :-
+    getVizinhos(C, Viz),
+    verificaLista(Viz, N, M),
+    !.
 
-% verifica a lista usando o método all_distinct do CLPFD
-verificaLista(L,N) :-
-    append(L,[N], Z),
-	all_distinct(Z).
+% Retorna true caso o valor não esteja presente na lista e vice-versa
+verificaLista([], _, _).
+verificaLista([H|T], N, M) :-
+    getP(H, M, P),
+    getV(P, V),
+    V \== N,
+    verificaLista(T, N, M),
+    !.
 
-suguru(matriz) :-
+% Realiza a verificação das lógicas do jogo: vizinhança e região. Checa se o valor está presente
+% na vizinhança da coordenada em questão e na sua região. Um wrapper para as funções verificaVizinho
+% e verificaRegiao
+verifica(C, N, M) :-
+    verificaRegiao(C, N, M),
+    verificaVizinhos(C, N, M),
+    !.
+
+% Retorna a primeira coordenada C que está vazia (ponto(0,X).
+verificaVazio(M, C) :-
+    (
+        nth0(0, M, L),
+        length(M, MI1),
+        length(L, MJ1),
+        MI is MI1 - 1,
+        MJ is MJ1 - 1,
+        between(0, MI, I),
+        between(0, MJ, J),
+        criarCoord(I, J, C),
+        getP(C, M, P),
+        getV(P, V),
+        V == 0
+    );
+    criarCoord(-1, -1, C).
+
+% Usando a função auxiliar verificar e a função findall, retorna uma lista de todos 
+verificaPossiveis(C, M, S) :-
+    getRegiaoFromMatriz(C, M, R),
+    length(R, Max),
+    findall(
+        N,
+        (
+            between(1, Max, N),
+            verifica(C, N, M)
+        ),
+        S
+    ).
+
+% resolve o puzzle
+suguru(coord(I,J), M, _, M) :-
+    length(M, L), I >= L, J >= L, !.
+suguru(coord(-1, -1), M, _, M) :- imprimeMatriz(M).
+suguru(_, _, [], []).
+suguru(C, M, [H|T], Rs) :-
+    getP(C, M, P1),
+    getR(P1, R1),
+    criarPonto(H, R1, P),
+    setaP(C, P, M, MN),
+    verificaVazio(MN, C2),
+    verificaPossiveis(C2, MN, S),
+    suguru(C2, MN, S, R2),
+    (
+        length(R2, 0),
+        suguru(C, M, T, _)
+    );
+    (
+        length(R2, T),
+        T \== 0,
+        Rs is R2
+    ).
+
+% entrada para solve
+suguruHelper :-
+    matriz(M),
+    verificaVazio(M, C),
+    verificaPossiveis(C, M, S),
+    suguru(C, M, S, _),
+    !.
+
+% imprime o valor do ponto
+imprimePonto(ponto(V, _)) :- write(V), tab(1).
+
+% imprime uma linha da matriz
+imprimeLinha([]) :- nl.
+imprimeLinha([H|T]) :-
+    imprimePonto(H),
+    imprimeLinha(T),
+    !.
+
+% imprime a matriz
+imprimeMatriz([]).
+imprimeMatriz([H|T]) :- 
+    imprimeLinha(H),
+    imprimeMatriz(T),
+    !.
+
+oldsuguru(matriz) :-
     matriz(M),
     getVFromMatriz(M, V),
     append(V, Vs), Vs ins 1..6.
@@ -193,3 +221,12 @@ blocks([], [], []).
 blocks([N1,N2,N3|Ns1], [N4,N5,N6|Ns2], [N7,N8,N9|Ns3]) :-
     all_distinct([N1,N2,N3,N4,N5,N6,N7,N8,N9]),
     blocks(Ns1, Ns2, Ns3).
+
+suguruTeste(C, M, [H|_], ML) :-
+    getP(C, M, P1),
+    getR(P1, R),
+    criarPonto(H, R, P),
+    setaP(C, P, M, ML).
+    %verificaVazio(ML, C2).
+    %verificaPossiveis(C2, ML, S).
+
